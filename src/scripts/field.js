7919928fca6aa677b2b739bde93ec9855e87609c
mckenziebyrdgranda <mckenziebyrd@me.com>
2022-01-31 20:25:41 -0600

    updated field code

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,24 +1,29 @@
-let field = []
+// let field = []
 
-const addPlant = []
+// const addPlant = []
 
-field.push(addPlant)
+// field.push(addPlant)
 
-console.log(field);
-
-
-const usePlants = () => { return field }
+// console.log(field);
 
 
-const createBellPepper = {
-    type: "Bell Pepper",
-    height: 24,
-    output: 3
-}
+// const usePlants = () => { return field }
 
-field.push(createBellPepper)
-field.push(createBellPepper)
 
-console.log(field)
+// const createBellPepper = {
+//     type: "Bell Pepper",
+//     height: 24,
+//     output: 3
+// }
 
-console.log(usePlants)
+// field.push(createBellPepper)
+// field.push(createBellPepper)
+
+// console.log(field)
+
+// console.log(usePlants)
+
+
+const field = []
+export const addPlant = (seed) => {field.push(seed)}
+export const usePlant = () => {return field }
